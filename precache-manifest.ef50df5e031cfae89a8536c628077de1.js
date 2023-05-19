ccb899aade8efae357044f36b811b4b2c9cd63ed
Cardeal <somoscardeal@proton.me>
2023-05-19 00:10:22 -0300

    Deploy React App to Github Pages

---
+++ precache-manifest.ef50df5e031cfae89a8536c628077de1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "49a2e94709567711f0d8",
+    "url": "/home/static/css/main.c8ce0ed8.chunk.css"
+  },
+  {
+    "revision": "49a2e94709567711f0d8",
+    "url": "/home/static/js/main.49a2e947.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/home/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/home/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "37f40d257a72e3da16cd",
+    "url": "/home/static/js/runtime~main.37f40d25.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/home/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "93b8f631259b2874c91c77ba137e8393",
+    "url": "/home/index.html"
+  }
+];
